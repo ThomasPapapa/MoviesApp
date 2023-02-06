12758131cc4e650d8a1a6350bbc38e92cbd4b8d4
Thomas Papapaschos <tpapapaschos@threenitas.com>
2023-02-06 02:07:50 +0200

    Simplified response model.

--- data/src/main/java/com/thomas/data/models/Show.kt
+++ data/src/main/java/com/thomas/data/models/Show.kt
@@ -4,50 +4,22 @@ package com.thomas.data.models
 import com.google.gson.annotations.SerializedName
 
 data class Show(
-    @SerializedName("averageRuntime")
-    val averageRuntime: Int?,
-    @SerializedName("dvdCountry")
-    val dvdCountry: Any?,
-    @SerializedName("ended")
-    val ended: String?,
-    @SerializedName("externals")
-    val externals: Externals?,
-    @SerializedName("genres")
-    val genres: List<String?>?,
     @SerializedName("id")
     val id: Int?,
-    @SerializedName("image")
-    val image: Image?,
-    @SerializedName("language")
-    val language: String?,
-    @SerializedName("_links")
-    val links: Links?,
     @SerializedName("name")
     val name: String?,
-    @SerializedName("network")
-    val network: Network?,
-    @SerializedName("officialSite")
-    val officialSite: String?,
-    @SerializedName("premiered")
-    val premiered: String?,
-    @SerializedName("rating")
-    val rating: Rating?,
-    @SerializedName("runtime")
-    val runtime: Int?,
-    @SerializedName("schedule")
-    val schedule: Schedule?,
-    @SerializedName("status")
-    val status: String?,
-    @SerializedName("summary")
-    val summary: String?,
     @SerializedName("type")
     val type: String?,
-    @SerializedName("updated")
-    val updated: Int?,
-    @SerializedName("url")
-    val url: String?,
-    @SerializedName("webChannel")
-    val webChannel: Any?,
-    @SerializedName("weight")
-    val weight: Int?
+    @SerializedName("language")
+    val language: String?,
+    @SerializedName("genres")
+    val genres: List<String>?,
+    @SerializedName("runtime")
+    val runtime: Int?,
+    @SerializedName("image")
+    val image: Image?,
+    @SerializedName("rating")
+    val rating: Rating?,
+    @SerializedName("summary")
+    val summary: String?
 )
